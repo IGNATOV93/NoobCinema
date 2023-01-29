8b0abfd2cdb7127c93889b188a25937f8d073e54
IGNATOV93 <ignatowja@gmail.com>
2023-01-29 15:07:53 +0300

    Добавьте файлы проекта.

---
+++ NoobCinema/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+
+public  class Viewer
+{
+
+    public Viewer(double price, int count)
+    {
+        price_of_ticket= price;
+        count_of_ticket= count;
+        Discount = discount;
+
+    }
+
+    public double price_of_ticket { get; set; }
+    public int count_of_ticket { get; set; }
+    public double discount;
+    public double Discount
+    {
+        get { return discount;}
+        set
+        {
+            discount = price_of_ticket*count_of_ticket;
+        }
+    }
+
+
+}
+
+public class Regular : Viewer
+{
+    public Regular(double price, int count, double discount) : base(price, count)
+    {
+    }
+}
+
+public class Student : Viewer
+{
+    public Student(double price, int count, double discount) : base(price, count)
+    {
+    }
+}
+
+public class Pensioner : Viewer
+{
+    public Pensioner(double price, int count, double discount) : base(price, count)
+    {
+    }
+}
+
+public class MainClass
+{
+    public static void Spliting()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите вид посетителя,кол-во его посещений,цену билета,сколько билетов покупает");
+            string input = Console.ReadLine();
+            string[] inputArray = input.Split(" ");
+            string visitor = inputArray[0];
+            if (visitor == "viewer")
+            {
+                Viewer viewer = new Viewer(double.Parse(inputArray[2]), int.Parse(inputArray[3]));
+                Console.WriteLine(viewer.discount); 
+
+            }
+            //
+            if (visitor == "regular")
+            {
+         //       Regular regular = new Regular();
+
+
+            }
+
+            if (visitor == "student")
+            {
+        //        Student student= new Student();
+            }
+
+            if (visitor == "pensioner")
+            {
+         //       Pensioner pensioner= new Pensioner();
+            }
+
+        }
+
+    }
+
+
+
+    public static void Main()
+    {
+        Spliting();
+    }
+}
+
