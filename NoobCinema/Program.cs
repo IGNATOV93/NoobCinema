cc131b9b46c41a2df3e3e616eabd5a2e1756aa51
IGNATOV93 <ignatowja@gmail.com>
2023-01-29 18:27:53 +0300

    сделал расчет для постоянных посетителей

--- NoobCinema/Program.cs
+++ NoobCinema/Program.cs
@@ -6,7 +6,6 @@ using System.Runtime.CompilerServices;
 
 public  class Viewer
 {
-
     public Viewer(double price, int count)
     {
         price_of_ticket= price;
@@ -15,7 +14,7 @@ public  class Viewer
     }
     public double price_of_ticket { get; set; }
     public int count_of_ticket { get; set; }
-    private double discount;
+    public double discount;
     public double Discount
     {
         get { return discount;}
@@ -28,15 +27,39 @@ public  class Viewer
 
 public class Regular : Viewer
 {
-    public Regular(double price, int count, double discount) : base(price, count)
+  public int numberOFvisits { get; set; }
+
+    public Regular(int numberOFvisits,double price, int count) : base(price, count)
     {
+        discount = CalcOFtickets(numberOFvisits,price,count);
+    }
+
+    public static double CalcOFtickets(int wasCountTickets,double buyCountTicket, int priceOFticket)
+    {
+        
+        double summa = 0; 
+        double price = 0;
+        int  wastickets=wasCountTickets;
+        while (wasCountTickets < wastickets+buyCountTicket)
+        {
+            int statsCount = (wasCountTickets+1)/ 10;
+            if (statsCount > 20) { statsCount = 20; }
+
+            double procent = (double)(100-statsCount) / 100;
+            price = priceOFticket * procent;
+            summa = summa + price;
+            wasCountTickets++;
+        }
+
+        return summa;
     }
 }
 
 public class Student : Viewer
 {
-    public Student(double price, int count, double discount) : base(price, count)
+    public Student(double price, int count, int discount) : base(price, count)
     {
+
     }
 }
 
@@ -65,26 +88,24 @@ public abstract class calculationOftickets
         if (visitor == "viewer")
         {
             Viewer viewer = new Viewer(double.Parse(inputArray[2]), int.Parse(inputArray[3]));
-           // Console.WriteLine(viewer.discount);
-           Print(viewer);
-
+            Print(viewer);
         }
-        //
+        
         if (visitor == "regular")
         {
-            //       Regular regular = new Regular();
-
+                  Regular regular = new Regular(int.Parse(inputArray[1]), int.Parse(inputArray[3]),int.Parse(inputArray[2]) );
+                  Print(regular);
 
         }
 
         if (visitor == "student")
         {
-            //        Student student= new Student();
+            
         }
 
         if (visitor == "pensioner")
         {
-            //       Pensioner pensioner= new Pensioner();
+            
         }
     }
 
@@ -98,10 +119,6 @@ public abstract class calculationOftickets
 
 public class MainClass
 {
-    
-
-
-
     public static void Main()
     {
         while (true)
