53ef90f52f5a27cd8f4aae9b13cb9c1a0c1d11c9
IGNATOV93 <ignatowja@gmail.com>
2023-01-30 23:13:49 +0300

    Закинул в регионы классы и методы

--- NoobCinema/Program.cs
+++ NoobCinema/Program.cs
@@ -4,13 +4,13 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
-
-public  class Viewer
+#region class Viewer
+public class Viewer
 {
     public Viewer(double price, int count)
     {
-        price_of_ticket= price;
-        count_of_ticket= count;
+        price_of_ticket = price;
+        count_of_ticket = count;
         Discount = discount;
     }
     public double price_of_ticket { get; set; }
@@ -18,50 +18,51 @@ public  class Viewer
     public double discount;
     public double Discount
     {
-        get { return discount;}
-        set
-        {
-            discount = price_of_ticket*count_of_ticket;
-        }
+        get { return discount; }
+        set { discount = price_of_ticket * count_of_ticket; }
     }
 }
+#endregion
 
+#region class Regular
 public class Regular : Viewer
 {
-  public int numberOFvisits { get; set; }
+    public int numberOFvisits { get; set; }
 
     public Regular(int numberOFvisits, double price, int count) : base(price, count)
     {
-        discount = CalcOFtickets(numberOFvisits,price,count);
+        discount = CalcOFtickets(numberOFvisits, price, count);
     }
 
-    public  virtual double CalcOFtickets(int wasCountTickets,double buyCountTicket, int priceOFticket)
+
+    public virtual double CalcOFtickets(int wasCountTickets, double buyCountTicket, int priceOFticket)
     {
-        
-        double summa = 0; 
-        double price = 0;
-        int  wastickets=wasCountTickets;
-        while (wasCountTickets < wastickets+buyCountTicket)
+        double summa = 0;
+        double price;
+        int wastickets = wasCountTickets;
+        while (wasCountTickets < wastickets + buyCountTicket)
         {
-            int statsCount = (wasCountTickets+1)/ 10;
+            int statsCount = (wasCountTickets + 1) / 10;
             if (statsCount > 20) { statsCount = 20; }
 
-            double procent = (double)(100-statsCount) / 100;
+            double procent = (double)(100 - statsCount) / 100;
             price = priceOFticket * procent;
-            summa = summa + price;
+            summa += price;
             wasCountTickets++;
         }
-
         return summa;
     }
 }
+#endregion
 
+#region class Student
 public class Student : Regular
 {
-    public Student(int numberOFvisits, int count,double price) : base(numberOFvisits, price, count)
+    public Student(int numberOFvisits, int count, double price) : base(numberOFvisits, price, count)
     {
         discount = CalcOFtickets(numberOFvisits, price, count);
     }
+
 
     public override double CalcOFtickets(int wasCountTickets, double priceOFticket, int buyCountTicket)
     {
@@ -70,45 +71,57 @@ public class Student : Regular
         while (wasCountTickets < wastickets + buyCountTicket)
         {
             int statsCount = (wasCountTickets + 1) % 3;
-
-            if (statsCount == 0 || statsCount == 1 || statsCount == 2)
-            {
-                summa+= price_of_ticket /2;
-            }
-            else
-            {
-                summa += priceOFticket;
-            }
-
+            if (statsCount == 0 || statsCount == 1 || statsCount == 2) summa += price_of_ticket / 2;
+            else summa += priceOFticket;
             wasCountTickets++;
         }
-
         return summa;
-
-
     }
 }
+#endregion
 
-public class Pensioner : Viewer
+#region class Pensioner
+public class Pensioner : Regular
 {
-    public Pensioner(double price, int count, double discount) : base(price, count)
+    public Pensioner(int numberOFvisits, int count, double price) : base(numberOFvisits, price, count)
     {
+        discount = CalcOFtickets(numberOFvisits, price, count);
+    }
+
+
+    public override double CalcOFtickets(int wasCountTickets, double priceOFticket, int buyCountTicket)
+    {
+        double summa = 0;
+        int wastickets = wasCountTickets;
+        while (wasCountTickets < wastickets + buyCountTicket)
+        {
+            int statsCount = (wasCountTickets + 1) % 5;
+            if (statsCount == 1 || statsCount == 2 || statsCount == 3 || statsCount == 4) summa += price_of_ticket / 2;
+            else summa += 0;
+            wasCountTickets++;
+        }
+        return summa;
     }
 }
+#endregion
 
-
-public abstract class calculationOftickets
+#region class CalculationOftickets
+public abstract class CalculationOftickets
 {
+    public static void StartsCalculetion()
     {
-        
-            Console.WriteLine("Введите вид посетителя,кол-во его посещений,цену билета,сколько билетов покупает");
+        while (true)
+        {
+            // Console.WriteLine("Введите вид посетителя,кол-во его посещений,цену билета,сколько билетов покупает");
             string input = Console.ReadLine();
             string[] inputArray = input.Split(" ");
-            MakingOFvisitor(inputArray);
+            UserIdentification(inputArray);
+        }
+
     }
 
-    #region void MakingOFvisitor
-    public static void MakingOFvisitor(string[] inputArray)
+    #region UserIdentification
+    public static void UserIdentification(string[] inputArray)
     {
         string visitor = inputArray[0];
         if (visitor == "viewer")
@@ -132,27 +145,27 @@ public abstract class calculationOftickets
 
         if (visitor == "pensioner")
         {
-
+            Pensioner pensioner = new Pensioner(int.Parse(inputArray[1]), int.Parse(inputArray[3]), int.Parse(inputArray[2]));
+            Print(pensioner);
         }
-    } 
+    }
     #endregion
 
     public static void Print(Viewer viewer)
     {
-        Console.WriteLine(viewer.Discount);
+        Console.WriteLine(viewer.Discount.ToString("#.##"));
     }
 
 }
 
 
+#endregion
+
 public class MainClass
 {
     public static void Main()
     {
-        while (true)
-        {
-             calculationOftickets.Spliting();
-        }
+        CalculationOftickets.StartsCalculetion();
     }
 }
 
